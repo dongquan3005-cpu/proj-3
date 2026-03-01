fb5d1384159364a3ffafe9f87d009707fdbc68f8
Quan Nguyen <qnguy8@uic.edu>
2026-03-01 00:47:29 -0600

    Finish pushback

--- circvector.h
+++ circvector.h
@@ -10,7 +10,7 @@ using namespace std;
 template <typename T>
 class CircVector {
  private:
-  T *data;
+  T* data;
   size_t vec_size;
   size_t capacity;
   size_t front_idx;
@@ -23,12 +23,23 @@ class CircVector {
    * Default constructor. Creates an empty `CircVector` with capacity 10.
    */
   CircVector() {
+    capacity = 10;
+    front_idx = 0;
+    vec_size = 0;
+    data = new T[10];
   }
 
   /**
    * Creates an empty `CircVector` with given capacity. Capacity must exceed 0.
    */
   CircVector(size_t capacity) {
+    if (capacity == 0) {
+      throw invalid_argument("capacity must be > 0");
+    }
+    this->capacity = capacity;
+    vec_size = 0;
+    front_idx = 0;
+    data = new T[this->capacity];
   }
 
   /**
@@ -36,14 +47,14 @@ class CircVector {
    * size is 0).
    */
   bool empty() const {
-    return false;
+    return vec_size == 0;
   }
 
   /**
    * Returns the number of elements in the `CircVector`.
    */
   size_t size() const {
-    return 0;
+    return vec_size;
   }
 
   /**
@@ -56,6 +67,22 @@ class CircVector {
    * Adds the given `T` to the back of the `CircVector`.
    */
   void push_back(T elem) {
+    if (vec_size == capacity) {
+      size_t new_capacity = capacity * 2;
+      T* new_data = new T[new_capacity];
+
+      for (size_t i = 0; i < vec_size; i++) {
+        new_data[i] = data[(front_idx + i) % capacity];
+      }
+
+      delete[] data;
+      data = new_data;
+      capacity = new_capacity;
+      front_idx = 0;
+    }
+    size_t physical_index = (front_idx + vec_size) % capacity;
+    data[physical_index] = elem;
+    vec_size++;
   }
 
   /**
@@ -86,6 +113,7 @@ class CircVector {
    * Destructor. Clears all allocated memory.
    */
   ~CircVector() {
+    delete[] data;
   }
 
   /**
@@ -93,8 +121,13 @@ class CircVector {
    *
    * If the index is invalid, throws `out_of_range`.
    */
-  T &at(size_t index) const {
-    return this->data[0];
+  T& at(size_t index) {
+    if (index >= vec_size) {
+      throw out_of_range("out of range index");
+    }
+    size_t physical_index = (front_idx + index) % capacity;
+
+    return data[physical_index];
   }
 
   /**
@@ -102,7 +135,7 @@ class CircVector {
    *
    * Must run in O(N) time.
    */
-  CircVector(const CircVector &other) {
+  CircVector(const CircVector& other) {
   }
 
   /**
@@ -111,7 +144,7 @@ class CircVector {
    *
    * Must run in O(N) time.
    */
-  CircVector &operator=(const CircVector &other) {
+  CircVector& operator=(const CircVector& other) {
     return *this;
   }
 
@@ -128,17 +161,17 @@ class CircVector {
    * Searches the `CircVector` for the first matching element, and returns its
    * index in the `CircVector`. If no match is found, returns "-1".
    */
-  size_t find(const T &target) {
+  size_t find(const T& target) {
     return 0;
   }
 
   /**
    * Removes the last occurrence of the given `value` from the `CircVector`.
    *
-   * For example, if a list was `[0, 1, 2, 1, 0, 1, 8]`, then a call to 
+   * For example, if a list was `[0, 1, 2, 1, 0, 1, 8]`, then a call to
    * `remove_last(1)` should change the list to be [0, 1, 2, 1, 0, 8]`.
-   * 
-   * If the given value is not found on the `CircVector`, throws an 
+   *
+   * If the given value is not found on the `CircVector`, throws an
    * `invalid_argument` exception.
    */
   void remove_last(const T& value) {
@@ -146,7 +179,7 @@ class CircVector {
 
   /**
    * Inserts the given `T` as a new element in the `CircVector` before
-   * the element currently stored in the given index. If the index is not 
+   * the element currently stored in the given index. If the index is not
    * a current valid index in the list, throws `out_of_range`.
    */
   void insert_before(size_t index, T elem) {
@@ -166,7 +199,7 @@ class CircVector {
    * Returns a pointer to the underlying memory managed by the `CircVec`.
    * For autograder testing purposes only.
    */
-  T *get_data() const {
+  T* get_data() const {
     return this->data;
   }
 
--- circvector_tests.cpp
+++ circvector_tests.cpp
@@ -6,3 +6,46 @@
 using namespace std;
 using namespace testing;
 
+TEST(CircVectorCore, ConstructorEmpty) {
+  CircVector<int> cv;
+  EXPECT_TRUE(cv.empty());
+  EXPECT_EQ(cv.size(), 0);
+}
+
+TEST(CircVectorCore, AtBasic) {
+  CircVector<int> cv;
+  cv.push_back(10);
+  cv.push_back(20);
+
+  EXPECT_EQ(cv.at(0), 10);
+  EXPECT_EQ(cv.at(1), 20);
+}
+
+TEST(CircVectorCore, AtOutOfRangeThrows) {
+  CircVector<int> cv;
+  cv.push_back(1);
+  EXPECT_THROW(cv.at(1), out_of_range);
+}
+
+TEST(CircVectorCore, PushBackBasic) {
+  CircVector<int> cv;
+  cv.push_back(1);
+  cv.push_back(2);
+  cv.push_back(3);
+
+  EXPECT_EQ(cv.at(0), 1);
+  EXPECT_EQ(cv.at(1), 2);
+  EXPECT_EQ(cv.at(2), 3);
+}
+
+TEST(CircVectorCore, PushBackResize) {
+  CircVector<int> cv(2);
+  cv.push_back(1);
+  cv.push_back(2);
+  cv.push_back(3);  // forces resize
+
+  EXPECT_EQ(cv.size(), 3);
+  EXPECT_EQ(cv.at(0), 1);
+  EXPECT_EQ(cv.at(1), 2);
+  EXPECT_EQ(cv.at(2), 3);
+}
